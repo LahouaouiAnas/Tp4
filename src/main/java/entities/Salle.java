ac44a0387fe15fb72676d249ee8d4d4243f1e376
LahouaouiAnas <lahouaouianas2@gmail.com>
2025-10-18 17:37:44 +0100

    tp4 hibernate initial commit

---
+++ src/main/java/entities/Salle.java
@@ -0,0 +1,47 @@
+package entities;
+
+import java.util.List;
+import javax.persistence.*;
+
+@Entity
+@Table(name = "salles")
+public class Salle {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String code;
+
+    @OneToMany(mappedBy = "salle", fetch = FetchType.EAGER)
+    private List<Machine> machines;
+
+    // Constructeurs, getters et setters
+
+    public Salle(String code) {
+    }
+
+    public Salle(String code, List<Machine> machines) {
+        this.code = code;
+        this.machines = machines;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public List<Machine> getMachines() {
+        return machines;
+    }
+
+    public void setMachines(List<Machine> machines) {
+        this.machines = machines;
+    }
+}
